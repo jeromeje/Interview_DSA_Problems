ad625933bfe24f562d8f60d79455d55a32ed0dd8
Jerome <156532175+jeromeje@users.noreply.github.com>
2025-06-03 09:11:45 +0530

    Create main2.py

---
+++ interview/1.leetcode_problems/001. Two sum /python /main2.py
@@ -0,0 +1,12 @@
+# hashing: o(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
